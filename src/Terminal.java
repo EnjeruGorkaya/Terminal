5285a41771bfbde707998f0b3126e693b026a2b3
EnjeruGorkaya <tanya.meow13@gmail.com>
2017-12-14 14:15:22 +0300

    Упс, медод забыла заимплементить

--- src/Terminal.java
+++ src/Terminal.java
@@ -5,4 +5,6 @@ public interface Terminal {
     void checkBalance() throws InterruptedException;
 
     public void withdrawMoney() throws InterruptedException;
+
+    public void  ReplenishMoney () throws InterruptedException;
 }
